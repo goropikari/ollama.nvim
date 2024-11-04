bcc4349f7f19131c02ede322db61c313d91a395e
goropikari <goropikari56@gmail.com>
2024-11-05 01:27:30 +0900

    change time interval

--- lua/ollama/spinner.lua
+++ lua/ollama/spinner.lua
@@ -11,7 +11,7 @@ function spinner.new()
 
   obj.start = function(self, bufnr)
     local timer = vim.uv.new_timer()
-    vim.uv.timer_start(timer, 0, 200, function()
+    vim.uv.timer_start(timer, 0, 100, function()
       vim.schedule(function()
         vim.api.nvim_buf_set_lines(bufnr, -2, -1, false, { spinner_symbols[self.idx] })
         self.idx = (self.idx % #spinner_symbols) + 1
